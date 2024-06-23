b044fa5568f8d2dc1d2f6a90c31eab94cd9abe0e
Manel Martos <mmartos@degirona.info>
2024-06-23 12:32:13 +0200

    Add more logs

--- composeApp/src/commonMain/kotlin/com/mmartosdev/photofx/materials/AssetContainer.kt
+++ composeApp/src/commonMain/kotlin/com/mmartosdev/photofx/materials/AssetContainer.kt
@@ -3,13 +3,14 @@ package com.mmartosdev.photofx.materials
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlin.math.max
-import kotlin.math.min
 
 @Composable
 fun AssetContainer(
@@ -21,7 +22,20 @@ fun AssetContainer(
             .fillMaxSize()
             .background(Color.Red.copy(alpha = 0.5f)),
     ) {
+        val scaleWidth = maxWidth / 680.dp
+        val scaleHeight = maxHeight / 404.dp
+        val scale = max(scaleWidth, scaleHeight)
+        Text(
+            text = """
+                        maxWidth = $maxWidth
+                        maxHeight = $maxHeight
+                        scaleWidth = $scaleWidth
+                        scaleHeight = $scaleHeight
+                        scale = $scale
+                    """.trimIndent(),
+            modifier = Modifier.align(Alignment.BottomStart),
+        )
         // Here, we are assuming that ideal screen size is 800x600
-        content.invoke(Modifier.scale(max(maxWidth / 800.dp, maxHeight / 600.dp)))
+        content.invoke(Modifier.scale(scale))
     }
 }
